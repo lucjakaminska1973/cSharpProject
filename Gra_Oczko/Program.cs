75a8f866a7eb54916469054e5ef1db3e4a406781
kamilu <lucja.kaminska@microsoft.wsei.edu.pl>
2020-07-06 00:49:46 +0200

    budowa klas

---
+++ Gra_Oczko/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_Oczko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
